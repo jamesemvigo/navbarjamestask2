dc0df16f76493835501aab69e65f5e9e529877ca
James Baby Emvigo <james.baby@emvigotech.com>
2022-08-15 19:03:39 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
-
-
 import React from 'react'
-import './Navbar.css'
+import Navbar from './Navbar'
 
-function Navbar() {
-    const buttons=['Home','About','Features','Pricing','How It Works','Contact','Login']
+function App() {
   return (
-    <div className="navbar">
-    <div className='header'>
-
-        <div className="left-container">
-            <img className='fleecard-image' src="http://fleecard.com/static/media/Fleecard_beta.4ffac123.png" alt="fleecard logo" />
-           
-        </div>
-
-
-
-       <div className="right-container">
-           <div className="log-buttons">
-            
-                {buttons.map((item)=>{
-                    return(
-                    <button className="buttonstyle" >{item}</button>
-                    )})}
-           </div>
-                
-       </div>
-    </div>
-    </div>
+    <div><Navbar></Navbar></div>
   )
 }
 
-export default Navbar
+export default App
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import './Navbar.css'
+
+// function Navbar() {
+//     const buttons=['Home','About','Features','Pricing','How It Works','Contact','Login']
+//   return (
+//     <div className="navbar">
+//     <div className='header'>
+
+//         <div className="left-container">
+//             <img className='fleecard-image' src="http://fleecard.com/static/media/Fleecard_beta.4ffac123.png" alt="fleecard logo" />
+           
+//         </div>
+
+
+
+//        <div className="right-container">
+//            <div className="log-buttons">
+            
+//                 {buttons.map((item)=>{
+//                     return(
+//                     <button className="buttonstyle" >{item}</button>
+//                     )})}
+//            </div>
+                
+//        </div>
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default Navbar
---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    const buttons=['Home','About','Features','Pricing','How It Works','Contact','Login']
+  return (
+    <div className="navbar">
+    <div className='header'>
+
+        <div className="left-container">
+            <img className='fleecard-image' src="http://fleecard.com/static/media/Fleecard_beta.4ffac123.png" alt="fleecard logo" />
+           
+        </div>
+
+
+
+       <div className="right-container">
+           <div className="log-buttons">
+            
+                {buttons.map((item)=>{
+                    return(
+                    <button className="buttonstyle" >{item}</button>
+                    )})}
+           </div>
+                
+       </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
